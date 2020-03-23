d799717f4f5ee6c03c5bb08e4f955f197d7d0f28
Brian L. Thornton <thorntonbl1@gmail.com>
2020-03-23 18:14:06 -0400

    Version 1.1 - 03/23/2020

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-# A San Francisco 49ers Trivia Game (An timers exercise built with JavaScript, jQuery, Bootstrap, HTML, & CSS)
+# San Francisco 49ers Trivia Game 
+###*An exercise about timers built with JavaScript, jQuery, Bootstrap, HTML, & CSS*
 
-((Under Construction: In the process of reworking projects from University of Richmond Coding Bootcamp))
+To begin the game press the start button at the center of the page header.
 
+- A timer will begin and a multiple choice question will appear on the screen. 
 
-Instructions:
+- You will have 10 seconds to answer the question correctly.
 
-To begin the game press the start button at the center of the page.
+- If you do answer correctly you will be alerted and 1 point will be added to your score. 
 
-Once you press the start button a 10 second timer will be set and the question will appear on the screen with four possible answers below it. 
+- If you answer incorrectly you will be alerted and 1 point will be subtracted from your score.
 
-You will have 10 seconds to answer correctly.
+- At the end of the game the total score is calculated to determine whether the player a true Red Blooded 49ers Faithful or just another Bandwagon Betty!
 
-If you do answer correctly you will be alerted and 1 point will be added to your score. 
-
-If you answer incorrectyl you will be alerted and 1 point will be subtracted from your score.
-
-At the end of the game the score will decide whether you are a true Red Blooded 49ers Faithful or just another Bandwagon Betty!
 
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,112 +1,92 @@
-//When the user clicks the start button display the first question from the questions array and start a 10 second timer.
-//If the user clicks the correct answer congratulate them, wait a few seconds, then display next question.
-//If the user clicks the wrong answer imsult them, wait a few seconds, then display the next question.
-//If the timer runs out show time's up, wait a few seconds, then display the next question.
-//When all 10 questions have been answered end the game and show the score and a restart button.
+// ---- VARIABLES --------------------------------------------------------------------------------------------------------------------
+var questions = 
+    [{
+        index: 0,
+        question: "How many Super Bowls have the 49ers won?",
+        choices: choices = [3, 4, 5, 6],
+        answer: this.choices[2],
+        correctCollapse: "Correct! The 49ers have won 5 Super Bowls and are widely considered the greatest team of all time! Facts!"
+    },
+    {
+        index: 1,
+        question: "In 1997, San Francisco drafted a Virginia Tech QB. Who was this successful college player who never caught on with the 49ers?",
+        choices: choices = ["Jim Druckenmiller", "Micheal Vick", "Bryan Randall", "Grant Noel"],
+        answer: this.choices[0],
+        correctCollapse: "And here we have a mention of the greatest college team of all time, the Virginia Tech Hokies!"
+    },
+    {
+        index: 2,
+        question: "What year were the 49ers founded?",
+        choices: choices = ["1945", "1946", "1947", "1948"],
+        answer: this.choices[1],
+        correctCollapse: "The Greatest Generation built the greatest football team. Not a coincidence."
+    },
+    {
+        index: 3,
+        question: "The 49ers were members of the _______________________ Football Conference before joining the NFL.",
+        choices: choices = ["All Conference", "All Players", "American", "All America"],
+        answer: this.choices[3],
+        correctCollapse: "Meeeerrrica!! All of it."
+    },
+    {
+        index: 4,
+        question: "Who made the famous catch to win the football game in the 1981 NFC Championship?",
+        choices: choices = ["Matt Bouza", "Dwight Clarke", "Jerry Rice", "Freddie Solomon"],
+        answer: this.choices[1],
+        correctCollapse: "You Dwight darn right he did!"
+    },
+    {
+        index: 5,
+        question: "How many times did 49er quarterback Joe Montana win a Super Bowl MVP?",
+        choices: choices = [1, 2, 3, 4],
+        answer: this.choices[2],
+        correctCollapse: "Tres, hombre"
+    },
+    {
+        index: 6,
+        question: "Where did the 49ers play for the first 25 seasons of the franchise?",
+        choices: choices = ["Candlestick Park", "Kezar Stadium", "Levi's Stadium", "AT&T Park"],
+        answer: this.choices[1],
+        correctCollapse: "Great salads. Oh?"
+    },
+    {
+        index: 7,
+        question: "Which of these players was not a member of the '88 49ers Super Bowl squad?",
+        choices: choices = ["Rikki Ellison", "Steve Young", "Wes Chandler", "Tom Cousineau"],
+        answer: this.choices[3],
+        correctCollapse: "Awww, should've been there! Rings!"
+    },
+    {
+        index: 8,
+        question: "Which 49er was Super Bowl XXIII MVP?",
+        choices: choices = ["Joe Montana", "Ronnie Lott", "Jerry Rice", "Steve Young"],
+        answer: this.choices[2],
+        correctCollapse: "Like white on Rice! Except he was black."
+    },
+    {
+        index: 9,
+        question: "How many NFL Super Bowls did Joe Montana win as quarterback for the 49ers?",
+        choices: choices = [1, 2, 3, 4],
+        answer: this.choices[3],
+        correctCollapse: "Correct answer: All of them!"
+    }];
 
-var questionOne = {
-    question: "How many Super Bowls have the 49ers won?",
-    choices: choices = [3, 4, 5, 6],
-    answer: this.choices[2],
-};
-
-var questionTwo = {
-    question: "In 1997, San Francisco drafted a Virginia Tech QB. Who was this successful college player who never caught on with the 49ers?",
-    choices: choices = ["Jim Druckenmiller", "Micheal Vick", "Bryan Randall", "Grant Noel"],
-    answer: this.choices[0],
-};
-
-var questionThree = {
-    question: "What year were the 49ers founded?",
-    choices: choices = ["1945", "1946", "1947", "1948"],
-    answer: this.choices[1],
-};
-
-var questionFour = {
-    question: "The 49ers were members of the _______________________ Football Conference before joining the NFL.",
-    choices: choices = ["All Conference", "All Players", "American", "All America"],
-    answer: this.choices[3],
-};
-
-var questionFive = {
-    question: "Who made the famous catch to win the football game in the 1981 NFC Championship?",
-    choices: choices = ["Matt Bouza", "Dwight Clarke", "Jerry Rice", "Freddie Solomon"],
-    answer: this.choices[1],
-};
-
-var questionSix = {
-    question: "How many times did 49er quarterback Joe Montana win a Super Bowl MVP?",
-    choices: choices = [1, 2, 3, 4],
-    answer: this.choices[2],
-};
-
-var questionSeven = {
-    question: "Where did the 49ers play for the first 25 seasons of the franchise?",
-    choices: choices = ["Candlestick Park", "Kezar Stadium", "Levi's Stadium", "AT&T Park"],
-    answer: this.choices[1],
-};
-
-var questionEight = {
-    question: "Which of these players was not a member of the '88 49ers Super Bowl squad?",
-    choices: choices = ["Rikki Ellison", "Steve Young", "Wes Chandler", "Tom Cousineau"],
-    answer: this.choices[3],
-};
-
-var questionNine = {
-    question: "Which 49er was Super Bowl XXIII MVP?",
-    choices: choices = ["Joe Montana", "Ronnie Lott", "Jerry Rice", "Steve Young"],
-    answer: this.choices[2],
-};
-
-var questionTen = {
-    question: "How many NFL Super Bowls did Joe Montana win as quarterback for the 49ers?",
-    choices: choices = [1, 2, 3, 4],
-    answer: this.choices[3],
-};
-
-var questions = [questionOne, questionTwo, questionThree, questionFour, questionFive, questionSix, questionSeven, questionEight, questionNine, questionTen];
-
-var correctCollapse = "Correct! The 49ers have won 5 Super Bowls and are widely considered the greatest team of all time! Facts!";
 var incorrectCollapse = "Ha! Wrong! I thought you said you were a fan!?";
-
 var timer = 10;
-
 var timerInterval;
+var index = 0;
+var id = '';
 
-var questionNumber = 0; 
 
-// Function to set the questions, choices, and accordian 
-function setQuestion() {
-    $('#timer').html('Time Remaining: ' + timer);
+// ---- TIMER FUNCTIONS -------------------------------------------------------------------------------------------------------------
 
-    $('#question').html('<h2>' + questions[questionNumber].question + '</h2>');
-
-    for (var i = 0; i < questions[questionNumber].choices.length; i++) {
-        $('#choice' + [i])
-            .html('<h2>' + questions[questionNumber].choices[i] + '</h2>')
-            .attr('val', questions[questionNumber].choices[i])
-    };
-
-    $('.list-group-item').attr('data-toggle', 'collapse');
-
-    for (var j = 0; j < questions[questionNumber].choices.length; j++) {
-
-        if (questions[questionNumber].choices[j] == questions[questionNumber].answer) {
-            $('#choiceCollapse' + [j]).html('<p>' + correctCollapse + '</p>');
-        } else {
-            $('#choiceCollapse' + [j]).html('<p>' + incorrectCollapse + '</p>');
-        };
-    
-    };
-};
-
-// Function to run the timer
+// ---- Function to run the timer
 function runTimer() {
     clearInterval(timerInterval);
     timerInterval = setInterval(setTimer, 1000);
 };
 
-// Function to reset and show timer on page
+// ---- Function to reset and show timer on page
 function setTimer() {
     timer--;
     $('#timer').html('Time Remaining: ' + timer);
@@ -117,70 +97,111 @@ function setTimer() {
     };
 };
 
-// Function to clear the time and alert the player that time is up.
+// ---- Function to clear the time and alert the player that time is up.
 function alertTimeout() {
+    $('#gameOver').modal({show: true});
     clearInterval(timerInterval);
-    alert('Times up!');
     timer = 10;
     $('#timer').html('Time Remaining: ' + timer);
 };
 
-// Function to reset the game when a player clicks the 'Restart' button
-function restartGame() {
-    /*
-        reset the timer
-        reset the scoreboard
-        go back to question 1
-    */
+
+// ---- GAME FUNCTIONS --------------------------------------------------------------------------------------------------------------
+
+// ---- Function to set the questions, choices, and accordian 
+function setQuestion() {
+    $('#timer').html('Time Remaining: ' + timer);
+    for (var i = 0; i < questions.length; i++) {
+        console.log(questions[index].question);
+        $('#question').html('<h2>' + questions[index].question + '</h2>');
+        for (var k = 0; k < questions[index].choices.length; k++) {
+            $('#choice' + [k])
+                .html('<h2>' + questions[index].choices[k] + '</h2>')
+                .attr('val', questions[index].choices[k])
+        };
+        $('.list-group-item').attr('data-toggle', 'collapse');
+        for (var j = 0; j < questions[index].choices.length; j++) {
+            if (questions[index].choices[j] == questions[index].answer) {
+                $('#choiceCollapse' + [j]).html('<p>' + questions[index].correctCollapse + '</p>');
+            } else {
+                $('#choiceCollapse' + [j]).html('<p>' + incorrectCollapse + '</p>');
+            };
+        }
+    }
 };
 
+// ---- Function to switch to the next question
+function nextQuestion() {
+    index++;
+    timer = 10;
+    setQuestion();
+    runTimer(); 
+};
+
+// ---- Function to reset the game when a player clicks the 'Restart' button
+function restartGame() {
+    index = 0;
+    timer = 10;
+    setQuestion();
+    runTimer();
+};
+
+// ---- Function to reload the page
+function reloadPage() {
+    location.reload(false);
+}
+
+
+// ---- MAIN GAME PLAY SECTION -------------------------------------------------------------------------------------------------------
 
 $(document).ready(function() {
-
-
+    // ---- Start button event to begin the timer and set the first question
     $("#startButton").on('click', function() {
         runTimer();
-        setQuestion();
+        setQuestion();   
     });
 
-    $('.list-group-item').on('click', function() {
-
-        playerChoice = $(this).attr('val');
-         
-        if (playerChoice == questions[questionNumber].answer) {
-            clearInterval(timerInterval);
-            $('#timer').html('Time Remaining: ' + timer);
-
-            setTimeout(nextQuestion, 3000);
-
-            function nextQuestion() {   
-                questionNumber++;
-                timer = 10;
-                setQuestion();
-                runTimer(); 
-            };
-        } else {
-            clearInterval(timerInterval);
-            $('#timer').html('Time Remaining: ' + timer);
-
-            setTimeout(nextQuestion, 3000);
-            
-            function nextQuestion() {
-                questionNumber++;
-                timer = 10;
-                setQuestion();
-                runTimer();
-            };
-        };     
-    });
-
+    // ---- On click event for 'Next Question' button 
     $('#nextQuestion').on('click', function() {
-        questionNumber++;
+        event.preventDefault();
+        index++;
         timer = 10;
         setQuestion();
         runTimer();
     });
 
-    $('#restartGame').on('click', restartGame());
+    // ---- On click event for 'Restart Game' button 
+    $('#restartGame').on('click', function() {
+        reloadPage();
+    });
 
+    // ---- On click event for 'Try Again' button 
+    $('#tryAgainButton').on('click', function() {
+        reloadPage();
+    });
+
+    // ---- On click event for 'Quit' anchor to link to 49ers.com 
+    $('#quitButton').on('click', function() {
+        reloadPage();
+    });
+
+    // ---- On click event to capture player choice and test their answer
+    $('.list-group-item').on('click', function() {
+        // Update playerChoice
+        playerChoice = $(this).attr('val');
+        console.log(playerChoice);
+        // Compare player choice to answer
+        if (playerChoice == questions[index].answer) {
+            // if player choice is correct...
+            clearInterval(timerInterval);
+            setTimeout(nextQuestion, 3000);
+            $('#timer').html('Time Remaining: ' + timer);
+        } else {
+            // else (if) player choice is incorrect
+            clearInterval(timerInterval);
+            setTimeout(nextQuestion, 3000);
+            $('#timer').html('Time Remaining: ' + timer);
+        };     
+    });
 });
+// ---- Main game-play section
